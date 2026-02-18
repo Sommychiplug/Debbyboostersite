14bddc5a4974225b61a699642cb7ecf6b3c5f075
Chima Chisom <Deborahchima122@gmail.com>
2026-02-18 15:09:50 +0100

    Fix Supabase URL and auth functions; add .gitignore

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Initialize Supabase client
 const supabase = window.supabase.createClient(
-  'https://tdiavdebbhzlkqyeyare.supabase.co',  // 
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRkaWF2ZGViYmh6bGtxeWV5YXJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzMDk1MTQsImV4cCI6MjA4Njg4NTUxNH0.ZBAZAKjOGSKCQUHTrp6PnTRvZdJu0aLurynO4Pz02gw'                          // <-- REPLACE THIS
+  'https://qiacnkzrcfuphohyvwcy',  
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpYWNua3pyY2Z1cGhvaHl2d2N5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzOTU1MDAsImV4cCI6MjA4Njk3MTUwMH0.jxAA51sJv64QYG8Li2-ynjpxvr4uJ7AsWxJGGixPnQ8'                          
 );
